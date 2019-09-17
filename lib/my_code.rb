440ee234c0de0b737ab5a03f271722e722be0bb3
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-09-17 17:42:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -44,7 +44,7 @@ def reduce_to_total(array, starting_point = 0)
   total = starting_point
 counter = 0 
 while counter < array.size do
-  final.push(array[counter] * -1)
+  total
   counter += 1
 end
 total
