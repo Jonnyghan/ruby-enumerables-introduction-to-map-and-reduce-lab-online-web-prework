b553bea8ee6b2d8cde73ed23091dd7255bf5d5f6
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-09-17 17:35:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,16 +20,24 @@ final
  end
  
  
- def map_to_double(source_array)
-   source_array = [1,2,3,-9]
-  source_array2 = source_array.map {|x| 2*x}
-  return source_array2
+ def map_to_double(array)
+   final = []
+counter = 0 
+while counter < array.size do
+  final.push(array[counter] * 2)
+  counter += 1
+end
+final
 end
 
-def map_to_square(source_array)
-  source_array = [1,2,3,-9]
-  source_array2 = source_array.map {|x|x*x}
-return source_array2
+def map_to_square(array)
+  final = []
+counter = 0 
+while counter < array.size do
+  final.push(array[counter] ** 2)
+  counter += 1
+end
+final
 end
 
 def reduce_to_total(source_array,starting_point)
